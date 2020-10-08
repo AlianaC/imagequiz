0e0730d09e91e7477e88256f120cfb8eda602ff4
Aliana Campas <alianacampas@Alianas-MacBook-Pro.local>
2020-10-08 02:32:25 -0700

    login testing

---
+++ imagequiz/src/components/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Home.css';
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: ''
+        };
+    }
+
+
+
+    render() {
+        let username = '';
+        const location = this.props.location;
+        if (location) {
+            if (location.state) {
+                if (location.state.user) {
+                    username = location.state.user;
+                }
+            }
+        }
+
+        return (
+            <div>
+                <div className="loginButton">
+                    {username.length > 0 ? username
+                        : <Link to='/login'>Login</Link>}
+                </div>
+                <div>hello from my homepage!</div>
+            </div>
+        );
+    }
+}
+
+export default Home;
