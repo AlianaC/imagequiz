33f9910c46618d36e908000ac8c33b057a7b128f
Aliana Campas <alianacampas@Alianas-MacBook-Pro.local>
2020-10-09 00:29:24 -0700

    test

--- imagequiz/src/App.js
+++ imagequiz/src/App.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Switch,
-  Route  
-} from "react-router-dom";
 import Home from './components/Home';
 import Login from './components/Login';
 import './App.css';
@@ -11,16 +6,7 @@ import './App.css';
 function App() {
   return (
     <div>hello, welcome to my page</div>
-    /*<BrowserRouter>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/login'>
-          <Login />
-        </Route>
-      </Switch>
-    </BrowserRouter>*/
+
   );
 }
 
