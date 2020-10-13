4e13b598f4d66c803380ab5694191493d834f799
Aliana Campas <alianacampas@Alianas-MacBook-Pro.local>
2020-10-13 13:41:14 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/imagequiz/precache-manifest.c5be4b5f7c1299ae4482327a5654e150.js"
+  "/imagequiz/precache-manifest.fedfc0b8886dfa931833a77f45d0edf9.js"
 );
 
 self.addEventListener('message', (event) => {
